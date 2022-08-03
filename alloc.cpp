93e261b81e539eeebfd641043fd32ef1116e49c0
Vinícius de Carvalho Pereira <95034279+nonekoy@users.noreply.github.com>
2022-08-02 22:01:59 -0300

    Add files via upload

---
+++ alloc.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int *pnt;
+    int num, i;
+    printf("Digite o tamanho da lista :  ");
+    scanf("%i",&num);
+    pnt = (int*) malloc(num*sizeof(int));
+    for (i=0; i<num;i++){
+        printf("Digite o %do numero: ", i+1);
+        scanf("%i", &pnt[i]);
+    }
+    printf("lista final: ");
+    for (i=0; i<num; i++){
+		printf("%i  ",pnt[i]);
+	}
+    
+    return 0;
+}
