b40d201eb9ed1326da387e8f48fb9563e3abd0c0
Vinícius de Carvalho Pereira <95034279+nonekoy@users.noreply.github.com>
2022-08-25 16:57:26 -0300

    Add files via upload

---
+++ aprovadoreprovado.js
@@ -0,0 +1,6 @@
+var nota1 = 6;
+var nota2 = 7;
+var nota3 = 7;
+
+const media = (nota1+nota2+nota3)/3;
+media >=7? console.log('Parabens você foi aprovado.'):console.log('Sinto muito você foi reprovado.');
