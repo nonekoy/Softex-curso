76cd7d16fb9efb2c9a1834386f5595e836af38d9
Vinícius de Carvalho Pereira <95034279+nonekoy@users.noreply.github.com>
2023-08-30 11:10:01 -0300

    Add files via upload

---
+++ modulo 2/calculomedia.js
@@ -0,0 +1,15 @@
+let nota1, nota2, nota3;
+let media;
+
+
+console.log("digite a nota 1: ");
+nota1 = Number(prompt());
+console.log("digite a nota 2: ");
+nota2 = Number(prompt());
+console.log("digite a nota 3: ");
+nota3 = Number(prompt());
+
+
+media = (nota1+nota2+nota3)/3;
+
+console.log("Sua média é : "+media)
