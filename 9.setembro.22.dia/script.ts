6bb8807497538b1431f7c20cd4d6e4bb8a35ba4f
Vinícius de Carvalho Pereira <95034279+nonekoy@users.noreply.github.com>
2023-10-10 13:51:38 -0300

    Add files via upload

---
+++ 9.setembro.22.dia/script.ts
@@ -0,0 +1,42 @@
+console.log("Olá, mundo!");
+
+class Pessoa{
+
+}
+
+/*
+interface ExercicioFavorito {
+  comecar(): void;
+}
+
+class Correr implements ExercicioFavorito {
+  comecar(): void {
+    console.log("Correr uma maratona...");
+  }
+}
+
+class Musculacao implements ExercicioFavorito {
+  comecar(): void {
+    console.log("Fazer musculação na academia...");
+  }
+}
+
+class Pessoa {
+  nome: string;
+  exercicioFavorito: ExercicioFavorito;
+  
+  constructor(nome: string, exercicioFavorito: ExercicioFavorito) {
+    this.nome = nome;
+    this.exercicioFavorito = exercicioFavorito;
+  }
+
+  exercitar(): void {
+    console.log(`${this.nome} decidiu:`);
+    this.exercicioFavorito.comecar();
+  }
+}
+
+const Gabriel = new Pessoa("Gabriel", new Correr());
+
+Gabriel.exercitar(); 
+*/
